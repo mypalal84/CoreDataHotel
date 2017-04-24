e66a6b4bde3a8fd30412053080c7fa9f6608fe0b
Alex Cahn <alexcahn@gmail.com>
2017-04-24 09:28:40 -0700

    Initial Commit

---
+++ CoreDataHotel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataHotel
+//
+//  Created by A Cahn on 4/24/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

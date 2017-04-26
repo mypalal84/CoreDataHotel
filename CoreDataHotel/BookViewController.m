a8b57a7c295a1ed34bb4da962e9ed051980ba548
Alex Cahn <alexcahn@gmail.com>
2017-04-25 17:41:51 -0700

    text fields set up

--- CoreDataHotel/BookViewController.m
+++ CoreDataHotel/BookViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BookViewController.h"
+#import "AutoLayout.h"
 
 @interface BookViewController ()
 
@@ -28,8 +29,33 @@
 
 -(void)setupTextFields{
     
-    UITextField *firstName = [[UITextField alloc]init];
+    self.firstNameTextField = [[UITextField alloc]init];
+    self.lastNameTextField = [[UITextField alloc]init];
+    self.emailTextField = [[UITextField alloc]init];
     
+    [self.firstNameTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.lastNameTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.emailTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self.view addSubview:self.firstNameTextField];
+    [self.view addSubview:self.lastNameTextField];
+    [self.view addSubview:self.emailTextField];
+    
+    [AutoLayout height:60 forView:self.firstNameTextField];
+    [AutoLayout width:200 forView:self.firstNameTextField];
+    [AutoLayout centerXFrom:self.firstNameTextField toView:self.view withOffset:0];
+    [AutoLayout topOffset:-100 fromViewTop:self.firstNameTextField toViewTop:self.lastNameTextField];
+    
+    [AutoLayout height:60 forView:self.lastNameTextField];
+    [AutoLayout width:200 forView:self.lastNameTextField];
+    [AutoLayout centerXFrom:self.lastNameTextField toView:self.view withOffset:0];
+    [AutoLayout centerYFrom:self.lastNameTextField toView:self.view withOffset:0];
+    
+    [AutoLayout height:60 forView:self.emailTextField];
+    [AutoLayout width:200 forView:self.emailTextField];
+    [AutoLayout centerXFrom:self.emailTextField toView:self.view withOffset:0];
+    [AutoLayout topOffset:140 fromViewTop:self.emailTextField toViewTop:self.lastNameTextField];
+
 }
 
 - (void)viewDidLoad {
